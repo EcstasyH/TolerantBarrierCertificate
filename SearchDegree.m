43ccc1493efddb95e5ab7a77c188e8fa98285641
han_repository <suhan@ios.ac.cn>
2023-02-02 14:05:13 +0800

    ra

--- SearchDegree.m
+++ SearchDegree.m
@@ -2,7 +2,7 @@ function SearchDegree()
 
 maxdeg = 12;
 azuma = 0;
-ra = 0;
+ra = 1;
 
 if ra == 0
     fprintf("================Safety==================\n");
@@ -11,17 +11,17 @@ else
 end
 
 fprintf("===============gamma1=================\n");
-for d =[2:maxdeg]
+for d = 2:maxdeg
     fprintf("Tring deg %d... ", d);
-    [sol, gamma1]  =TolerantBC(d, azuma, ra);   
+    [~, ~]  =TolerantBC(d, azuma, ra);   
 end
 fprintf("=====================================\n");
 
 
 azuma = 1;
 fprintf("===============gamma2=================\n");
-for d =[2:maxdeg]
+for d = 2:maxdeg
     fprintf("Tring deg %d ...", d);
-    [sol, gamma2]  =TolerantBC(d, azuma,ra);   
+    [~, ~]  =TolerantBC(d, azuma,ra);   
 end
 fprintf("=====================================\n");
