ef4831e5802d4695315786deb0438d3865857f81
EcstasyH <wuhao164@mails.ucas.ac.cn>
2023-02-04 18:39:29 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# TolerantBarrierCertificate
+# Tolerant Barrier Certificate
+
 Synthesizing Tolerant-BC for stochastic dynamical systems.
+
+## Get Started
+
+1. Our implementation is based on the sum-of-squares module **YALMP** and the SDP solver **MOSEK**. Make sure they are installed correctly.
+2. `git clone` this project and add the folder to the path of the matlab environment.
+3. run `SearchDegree.m`
+4. to run different examples, change the following code in `TolerantBC.m` file: `[dim,f1,f2,f3,f4,f5,f6,S0_f,Su_f,St_f,Sd_f] = Ex1d_randomwalk(vars_total,ra);`
+
--- SearchDegree.m
+++ SearchDegree.m
@@ -1,8 +1,10 @@
 function SearchDegree()
 
-maxdeg = 16;
-azuma = 0;
-ra = 0;
+% maxdeg: search range, set 16,12,10,6 for system with dimension 1,2,3,4
+% ra: =0 safety =1 reach-avoid
+maxdeg = 16; %6,10,12,16
+ra = 0; 
+
 
 if ra == 0
     fprintf("================Safety==================\n");
@@ -10,6 +12,7 @@ else
     fprintf("==============Reach-Avoid===============\n");
 end
 
+azuma = 0;
 fprintf("===============gamma1=================\n");
 tic
 for d = 2:maxdeg
@@ -19,7 +22,6 @@ for d = 2:maxdeg
 toc
 end
 fprintf("=====================================\n");
-
 
 azuma = 1;
 fprintf("===============gamma2=================\n");
